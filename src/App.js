ab4b7dbec6ec2c4250d0a238ae172f62f6b966cb
Brodie Barton <brodiebarton@gmail.com>
2020-08-04 15:00:00 -0400

    general ui design implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import Dashboard from './Components/Dashboard';
+import 'normalize.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-	  	
-      </header>
+	  	<Dashboard />
     </div>
   );
 }
